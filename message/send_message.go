b3aa761b0f6bb3fb858459ddbb35306a375ffc15
lenye <pyweb@qq.com>
2017-06-27 16:00:57 +0800

    修改access_token参数

--- message/send_message.go
+++ message/send_message.go
@@ -15,8 +15,8 @@ import (
 const sendMessageURL = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s"
 
 // SendMessage 发送消息. 应用支持推送文本、图片、视频、文件、图文等类型
-func SendMessage(body *bytes.Buffer, access_token string, HTTPClientConnectTimeout, HTTPClientRequestTimeout time.Duration) ([]byte, error) {
-	resp, err := api.NewHttpClient(HTTPClientConnectTimeout, HTTPClientRequestTimeout).HTTPPostJSON(fmt.Sprintf(sendMessageURL, access_token), body)
+func SendMessage(body *bytes.Buffer, accessToken string, HTTPClientConnectTimeout, HTTPClientRequestTimeout time.Duration) ([]byte, error) {
+	resp, err := api.NewHttpClient(HTTPClientConnectTimeout, HTTPClientRequestTimeout).HTTPPostJSON(fmt.Sprintf(sendMessageURL, accessToken), body)
 	if err != nil {
 		glog.Error(errors.Wrap(err, "qyapi SendMessage"))
 		return nil, api.Err{Code: http.StatusInternalServerError, Text: "INTERNAL_ERROR"}
