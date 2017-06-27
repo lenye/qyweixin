ba33c95a101ec85f66c1c7cc87004564ab720b84
lenye <pyweb@qq.com>
2017-06-27 21:22:15 +0800

    version string

--- internal/version/binary.go
+++ internal/version/binary.go
@@ -5,8 +5,10 @@ import (
 	"runtime"
 )
 
+//Binary 版本号
 const Binary = "0.01"
 
+//String 版本号
 func String(app string) string {
 	return fmt.Sprintf("%s/%s (built with %s %s for %s/%s)", app, Binary, runtime.Compiler, runtime.Version(), runtime.GOOS, runtime.GOARCH)
 }
