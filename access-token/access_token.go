520922f9091898459c28258c24426df13361f952
lenye <pyweb@qq.com>
2019-12-27 14:56:24 +0800

    time NewTicker更换为NewTimer

--- access-token/access_token.go
+++ access-token/access_token.go
@@ -13,16 +13,16 @@ import (
 )
 
 const (
-	retryInterval = 10 * 1000                                                              //每隔10秒重试
-	tokenURL      = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=%s&corpsecret=%s" //企业微信token url
+	retryInterval = 10 * 1000                                                              // 每隔10秒重试
+	tokenURL      = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=%s&corpsecret=%s" // 企业微信token url
 )
 
-//凭证
+// 凭证
 type AccessToken struct {
-	Ticket    string    `json:"access_token"` //凭证
-	ExpiresIn int64     `json:"expires_in"`   //凭证有效时间，单位：秒
-	NextGet   int64     `json:"-"`            //下次取凭证时间
-	CreateAt  time.Time `json:"create_at"`    //取得凭证的时间
+	Ticket    string    `json:"access_token"` // 凭证
+	ExpiresIn int64     `json:"expires_in"`   // 凭证有效时间，单位：秒
+	NextGet   int64     `json:"-"`            // 下次取凭证时间
+	CreateAt  time.Time `json:"create_at"`    // 取得凭证的时间
 }
 
 type AccessTokenClient struct {
@@ -40,20 +40,20 @@ func NewAccessTokenClient(connectTimeout time.Duration, requestTimeout time.Dura
 	return p
 }
 
-//Load
+// Load
 func (p *AccessTokenClient) Load() *AccessToken {
 	return p.ticket.Load().(*AccessToken)
 }
 
-//swapTicket
+// swapTicket
 func (p *AccessTokenClient) SwapTicket(ticket *AccessToken) {
 	p.ticket.Store(ticket)
 }
 
-//getAccessToken
+// getAccessToken
 func (p *AccessTokenClient) getAccessToken(appId, appSecret string) (*AccessToken, error) {
 	accessToken := p.Load()
-	//清除过期access-token
+	// 清除过期access-token
 	if accessToken.Ticket != "" && int64(time.Now().Sub(accessToken.CreateAt).Seconds()) >= accessToken.ExpiresIn {
 		accessToken.Ticket = ""
 		accessToken.ExpiresIn = 0
@@ -76,7 +76,7 @@ func (p *AccessTokenClient) getAccessToken(appId, appSecret string) (*AccessToke
 		return accessToken, errors.Wrap(err, "getAccessToken json.Unmarshal")
 	}
 	newAccessToken.CreateAt = time.Now()
-	//刷新策略
+	// 刷新策略
 	switch {
 	case newAccessToken.ExpiresIn >= 60*60:
 		newAccessToken.NextGet = (newAccessToken.ExpiresIn - 30*60) * 1000
@@ -99,29 +99,32 @@ func (p *AccessTokenClient) getAccessToken(appId, appSecret string) (*AccessToke
 	return accessToken, nil
 }
 
-//Loop
+// Loop
 func (p *AccessTokenClient) Loop(appId, appSecret string) {
-	var refreshInterval time.Duration
+	var beatInterval time.Duration
 
 	newAccessToken, err := p.getAccessToken(appId, appSecret)
 	if err != nil {
 		glog.Error(err)
 	}
-	refreshInterval = time.Duration(newAccessToken.NextGet) * time.Millisecond
-	glog.Infof("next access-token time.NewTicker=%v", refreshInterval)
-	timeTicker := time.NewTicker(refreshInterval)
-
+	beatInterval = time.Duration(newAccessToken.NextGet) * time.Millisecond
+	glog.Infof("next access-token beat interval=%v", beatInterval)
+	beatTimer := time.NewTimer(beatInterval)
+	defer func() {
+		if !beatTimer.Stop() {
+			<-beatTimer.C
+		}
+	}()
 	for {
 		select {
-		case <-timeTicker.C:
+		case <-beatTimer.C:
 			newAccessToken, err := p.getAccessToken(appId, appSecret)
 			if err != nil {
 				glog.Error(err)
 			}
-			refreshInterval = time.Duration(newAccessToken.NextGet) * time.Millisecond
-			glog.Infof("next access-token time.NewTicker=%v", refreshInterval)
-			timeTicker.Stop()
-			timeTicker = time.NewTicker(refreshInterval)
+			beatInterval = time.Duration(newAccessToken.NextGet) * time.Millisecond
+			glog.Infof("next access-token beat interval=%v", beatInterval)
+			beatTimer.Reset(beatInterval)
 		case <-p.QuitChan:
 			goto exit
 		}
@@ -129,5 +132,4 @@ func (p *AccessTokenClient) Loop(appId, appSecret string) {
 
 exit:
 	glog.Info("exiting access-token Loop")
-	timeTicker.Stop()
 }
